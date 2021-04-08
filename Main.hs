b88c26349311ff6025322f54d69c16d99aaa5dbf
noahma <noahfierz@gmail.com>
2021-04-07 21:13:09 -0700

    added switch control flow function

--- Main.hs
+++ Main.hs
@@ -1,6 +1,7 @@
 import Control.Monad.State
 import Data.Function
 import qualified Data.Map as Map
+import qualified Data.List as Ls 
 import Object
 import World
 
@@ -12,27 +13,29 @@ r3 = room "three" "" [] []
 
 w = giveObj r1 >> giveObj r2
 
-test = do 
-    giveObj r3
-    takeObj "one"
-
+switch :: (Eq a) => a -> [(a, b)] -> b -> b
+switch val branches def = 
+    maybe def snd (Ls.find ((== val) . fst) branches)
 
 loop room = do
     line <- liftIO $ do 
         putStrLn (name room)
-        getLine 
-    case line of 
-        "stop" -> return ()
-        name -> do 
-            maybeRoom <- takeObj name
+        getLine    
+    switch line [
+        ("stop", return ()),
+        (name room, do 
+            liftIO $ putStrLn $ "Already in " ++ line
+            loop room)]
+        $ do 
+            maybeRoom <- takeObj line
             case maybeRoom of
                 Just newRoom -> do 
                     giveObj room
                     loop newRoom
                 Nothing -> do
-                    liftIO (putStrLn $
-                        "can't go to " ++ name)
-                    loop room 
+                    (liftIO $ putStrLn $
+                        "can't go to " ++ line)
+                    loop room
 
 main = do 
     runStateT (w >> loop r3) Map.empty
